3a6786ad417f65ed192981da2d47f5f7206eeab4
aohs43 <alex.ochs@mni.thm.de>
2023-02-01 13:32:33 +0100

    simple metamask login, finally...

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY as string,
   authentication: {
-    domain: 'http://localhost:3000',
+    domain: 'BlockEstate.tech',
     uri: process.env.NEXTAUTH_URL as string,
     timeout: 120,
   },
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,66 +1,53 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { signIn, useSession } from "next-auth/react";
-import { useAccount, useSignMessage, useNetwork } from "wagmi";
-import { useEffect } from "react";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { signIn } from "next-auth/react";
+import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
 import { useRouter } from "next/router";
-import axios from "axios";
 import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
+import { Button, Heading } from "@chakra-ui/react";
 
 function SignIn() {
-  const { isConnected, address } = useAccount();
-  const { chain } = useNetwork();
-  const { status } = useSession();
+  const { connectAsync } = useConnect();
+  const { disconnectAsync } = useDisconnect();
+  const { isConnected } = useAccount();
   const { signMessageAsync } = useSignMessage();
-  const { push } = useRouter();
   const { requestChallengeAsync } = useAuthRequestChallengeEvm();
+  const { push } = useRouter();
 
-  useEffect(() => {
-    const handleAuth = async () => {
-      const userData = { address, chain: chain?.id, network: "evm" };
-
-      /*const { data } = await axios.post("/api/auth/request-message", userData, {
-        headers: {
-          "content-type": "application/json",
-        },
-      });*/
-
-      //const message = data.message;
-      const message = await requestChallengeAsync({
-        address: userData.address as string,
-        chainId: userData.chain as number,
-      });
-
-      const signature = await signMessageAsync({
-        message: message?.message as string,
-      });
-
-      // redirect user after success authentication to '/user' page
-      const response = await signIn("credentials", {
-        message,
-        signature,
-        redirect: false,
-        callbackUrl: "/user",
-      });
-
-      if (!response) {
-        return;
-      }
-
-      /**
-       * instead of using signIn(..., redirect: "/user")
-       * we get the url from callback and push it to the router to avoid page refreshing
-       */
-      push(response.url!);
-    };
-    if (status === "unauthenticated" && isConnected) {
-      handleAuth();
+  const handleAuth = async () => {
+    if (isConnected) {
+      await disconnectAsync();
     }
-  }, [status, isConnected]);
+
+    const { account, chain } = await connectAsync({
+      connector: new MetaMaskConnector(),
+    });
+
+    const { message }: any = await requestChallengeAsync({
+      address: account,
+      chainId: chain.id,
+    });
+
+    const signature = await signMessageAsync({ message });
+
+    // redirect user after success authentication to '/user' page
+    const { url }: any = await signIn("moralis-auth", {
+      message,
+      signature,
+      redirect: false,
+      callbackUrl: "/user",
+    });
+
+    /*
+     * instead of using signIn(..., redirect: "/user")
+     * we get the url from callback and push it to the router to avoid page refreshing
+     */
+    push(url);
+  };
 
   return (
     <div>
-      <h3>Web3 Authentication</h3>
-      <ConnectButton />
+      <Heading>Web3 Authentication</Heading>
+      <Button onClick={handleAuth}>Authenticate via Metamask</Button>
     </div>
   );
 }
